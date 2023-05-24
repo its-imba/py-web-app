37f8cb8fedd3420e2c48f2f36e3c91868bf99e82
its-imba <its.sam.imba@gmail.com>
2023-05-24 11:39:03 +0100

    added update notes functionality v1

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
@@ -22,8 +22,10 @@ def home():
             db.session.commit()
             flash('Note added', category='success')
 
+    # Fetch all notes for the current user
+    notes = Note.query.filter_by(user_id=current_user.id).all()
 
-    return render_template("home.html", user=current_user)
+    return render_template("home.html", user=current_user, notes=notes)
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
@@ -36,3 +38,47 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
+
+@views.route('/update-note', methods=['POST'])
+@login_required
+def update_note():
+    data = request.get_json()
+    note_id = data['noteId']
+    updated_note = data['updatedText']
+    note = Note.query.get(note_id)
+
+    if note:
+        if note.user_id == current_user.id:
+            if len(updated_note) < 1:
+                flash('Note is too short', category='error')
+            else:
+                note.data = updated_note
+                db.session.commit()
+                flash('Note updated', category='success')
+                return jsonify(message='Note updated')
+
+    flash('Note not found or you do not have permission to edit it', category='error')
+    return jsonify(message='Error')
+
+@views.route('/edit-note/<int:note_id>', methods=['GET', 'POST'])
+@login_required
+def edit_note(note_id):
+    note = Note.query.get(note_id)
+
+    if note:
+        if note.user_id == current_user.id:
+            if request.method == 'POST':
+                new_note = request.json.get('updatedNote')
+
+                if len(new_note) < 1:
+                    flash('Note is too short', category='error')
+                else:
+                    note.data = new_note
+                    db.session.commit()
+                    flash('Note updated', category='success')
+                    return jsonify(message='Note updated')
+
+            return render_template('edit_note.html', user=current_user, note=note)
+
+    flash('Note not found or you do not have permission to edit it', category='error')
+    return redirect(url_for('views.home'))
