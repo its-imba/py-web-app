d88b84d8add0a30f9954879dadbd4255b79e2527
its-imba <its.sam.imba@gmail.com>
2023-05-24 13:32:05 +0100

    cleaning up code & adding log out success flash

--- website/__init__.py
+++ website/__init__.py
@@ -4,20 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-db = SQLAlchemy()
 DB_NAME = 'database.db'
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created DB')
-
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dkd93kshaghj429'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Specify the file path within the "backend" directory
     database_path = os.path.join(os.path.dirname(app.root_path), 'backend', DB_NAME)
@@ -25,14 +18,15 @@ def create_app():
 
     db.init_app(app)
 
+    with app.app_context():
+        from . import models  # Import models here to avoid circular import
+        db.create_all()
+
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix = '/')
-    app.register_blueprint(auth, url_prefix = '/')
-
-    with app.app_context():
-        db.create_all()
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -43,4 +37,3 @@ def create_app():
         return models.User.query.get(int(id))
 
     return app
-
